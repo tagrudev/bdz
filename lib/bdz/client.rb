cb94d274184e75d1bc97814e39d207e1dfdd4cf6
Todor Grudev <tagrudev@gmail.com>
2012-08-21 16:16:27 +0300

    Initializing

--- lib/bdz/client.rb
+++ lib/bdz/client.rb
@@ -1,16 +1,14 @@
 module Bdz
   class Client
     attr_reader :params
-    attr_reader :defaults 
     def initialize
       @params = {}
-      @defaults = { :action => 'listOptions', :dep_arr => '1', :time_from => '00:00', :time_to => '24:00', :all_cats => 'checked', 
-        :cardId => '30', :sort_by => '0'}
     end
 
     private
     def build_params(params)
-      @params = params.merge(@defaults)
+      @params = params.merge({ :action => 'listOptions', :dep_arr => '1', :time_from => '00:00', :time_to => '24:00', :all_cats => 'checked', 
+        :cardId => '30', :sort_by => '0'})
     end
   end
 end
